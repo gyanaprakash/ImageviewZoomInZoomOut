1e0ab7b93c959f8dabac4d9684beaf29a5f74627
gyana prakash gouda <gyanaprakashgouda471@gmail.com>
2014-11-04 10:13:31 +0530

    Initial Commit

---
+++ ImageviewZoomInZoomOut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageviewZoomInZoomOut
+//
+//  Created by Bsetecip10 on 04/11/14.
+//  Copyright (c) 2014 Bsetecip10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageviewZoomInZoomOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageviewZoomInZoomOut
+//
+//  Created by Bsetecip10 on 04/11/14.
+//  Copyright (c) 2014 Bsetecip10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
